e19f0500e9fa97287f9536d5ed6723244f1ce1f1
Andy Wong <awong@propelmarketing.com>
2016-11-01 17:22:47 -0400

    Adding delegate method to protect final default row from reordering.

--- Homepwner/Homepwner/ItemsViewController.swift
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -98,4 +98,8 @@ class ItemsViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
         return "Remove"
     }
+
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return indexPath.row == itemStore.allItems.count ? false : true
+    }
 }
