68c16c75b1f393596c5ba26b0eba7af6a426ce3c
Andy Wong <awong@propelmarketing.com>
2016-11-01 15:11:35 -0400

    Adding ability to delete, reoder rows.

--- Homepwner/Homepwner/ItemStore.swift
+++ Homepwner/Homepwner/ItemStore.swift
@@ -11,17 +11,27 @@ import UIKit
 class ItemStore {
     var allItems = [Item]()
 
-    init() {
-        for _ in 0..<5 {
-            createItem()
-        }
-    }
-
     func createItem() -> Item {
         let newItem = Item(random: true)
 
         allItems.append(newItem)
 
         return newItem
+    }
+
+    func removeItem(_ item: Item) {
+        if let index = allItems.index(of: item) {
+            allItems.remove(at: index)
+        }
+    }
+
+    func moveItemAtIndex(_ fromIndex: Int, toIndex: Int) {
+        if fromIndex == toIndex {
+            return
+        }
+
+        let movedItem = allItems[fromIndex]
+        allItems.remove(at: fromIndex)
+        allItems.insert(movedItem, at: toIndex)
     }
 }
